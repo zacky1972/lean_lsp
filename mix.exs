25bc80ce43588b49acf92f5080be0021728aa2d3
Susumu Yamazaki <zacky1972@gmail.com>
2026-05-13 19:50:48 +0900

    build: install docker_availability

--- mix.exs
+++ mix.exs
@@ -80,6 +80,7 @@ defmodule LeanLsp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:docker_availability, "~> 1.0"},
       {:nstandard, "~> 0.3"},
       {:ex_doc, "~> 0.40", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
