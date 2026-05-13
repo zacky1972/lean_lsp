eab53c971090d6e6f24469fd76172132853547ed
Susumu Yamazaki <zacky1972@gmail.com>
2026-05-13 19:47:18 +0900

    build: install and set up nstandard

--- mix.exs
+++ mix.exs
@@ -9,7 +9,12 @@ defmodule LeanLsp.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description:
-        "an OTP-native Elixir client for running and querying Lean's language server, with Docker-backed runtime support."
+        "an OTP-native Elixir client for running and querying Lean's language server, with Docker-backed runtime support.",
+      name: "Lean lsp",
+      docs: docs(),
+      package: package(),
+      aliases: aliases(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -20,9 +25,66 @@ defmodule LeanLsp.MixProject do
     ]
   end
 
+  def docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
+  def package do
+    [
+      name: :lean_lsp,
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/TODO/lean_lsp"}
+    ]
+  end
+
+  def aliases do
+    [
+      check: [
+        "hex.audit",
+        "compile --warnings-as-errors --force",
+        "format --check-formatted",
+        "credo",
+        "deps.unlock --check-unused",
+        "spellweaver.check",
+        "dialyzer"
+      ],
+      precommit: [
+        "hex.audit",
+        "compile --warnings-as-errors --force",
+        "format",
+        "credo",
+        "deps.unlock --unused",
+        "spellweaver.check",
+        "dialyzer",
+        "test"
+      ]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [precommit: :test]
+    ]
+  end
+
+  def dialyzer do
+    [
+      plt_add_apps: [:mix],
+      ignore_warnings: ".dialyzer_ignore.exs"
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:nstandard, "~> 0.3"},
+      {:ex_doc, "~> 0.40", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:spellweaver, "~> 0.1", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
--- test/lean_lsp_test.exs
+++ test/lean_lsp_test.exs
@@ -1,8 +1,4 @@
 defmodule LeanLspTest do
   use ExUnit.Case
   doctest LeanLsp
-
-  test "greets the world" do
-    assert LeanLsp.hello() == :world
-  end
 end
