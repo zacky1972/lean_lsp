ab6d5a1b4badcdb44c325e6f92490312cc731d49
Susumu Yamazaki <zacky1972@gmail.com>
2026-05-15 08:50:35 +0900

    fix: issue on docker

--- test/lean_tool_empty_execution_integration_test.exs
+++ test/lean_tool_empty_execution_integration_test.exs
@@ -10,39 +10,39 @@ defmodule LeanLsp.LeanToolEmptyExecutionIntegrationTest do
   @docker_image_env "LEAN_LSP_TEST_DOCKER_IMAGE"
 
   setup_all do
-    docker = System.find_executable("docker") || flunk("docker executable was not found on PATH")
-
-    assert_command_success!(docker, ["info"], @docker_info_timeout)
-
-    {:ok, docker: docker, image: lean_docker_image()}
+    assert_command_success!(["info"], @docker_info_timeout)
+    {:ok, image: lean_docker_image()}
   end
 
   @tag timeout: @docker_run_timeout + @docker_info_timeout + 5_000
   test "configured Lean Docker image can execute lean with no arguments", %{
-    docker: docker,
     image: image
   } do
-    assert_bare_tool_execution!(docker, image, "lean")
+    assert_bare_tool_execution!(image, "lean")
   end
 
   @tag timeout: @docker_run_timeout + @docker_info_timeout + 5_000
   test "configured Lean Docker image can execute lake with no arguments", %{
-    docker: docker,
     image: image
   } do
-    assert_bare_tool_execution!(docker, image, "lake")
+    assert_bare_tool_execution!(image, "lake")
   end
 
   defp lean_docker_image do
     System.get_env(@docker_image_env, RuntimeConfig.default_docker_image())
   end
 
-  defp assert_bare_tool_execution!(docker, image, tool) do
+  defp assert_bare_tool_execution!(image, tool) do
     args = ["run", "--rm", "--entrypoint", "", image, tool]
+    assert_command_success!(args, @docker_info_timeout)
+  end
 
-    case run_command(docker, args, @docker_run_timeout) do
-      {:ok, {output, exit_status}} when exit_status in 0..124 ->
-        assert_normal_container_command_exit!(docker, args, output, exit_status)
+  defp assert_command_success!(args, timeout) do
+    {:ok, docker} = DockerAvailability.executable()
+
+    case run_command(docker, args, timeout) do
+      {:ok, {_output, exit_status}} when exit_status in 0..124 ->
+        :ok
 
       {:ok, {output, exit_status}} ->
         flunk("""
@@ -67,55 +67,6 @@ defmodule LeanLsp.LeanToolEmptyExecutionIntegrationTest do
         flunk("""
         container command timed out after #{timeout}ms: #{format_command(docker, args)}
         """)
-    end
-  end
-
-  defp assert_command_success!(executable, args, timeout) do
-    case run_command(executable, args, timeout) do
-      {:ok, {_output, 0}} ->
-        :ok
-
-      {:ok, {output, exit_status}} ->
-        flunk("""
-        command failed: #{format_command(executable, args)}
-
-        exit status:
-        #{exit_status}
-
-        output:
-        #{output}
-        """)
-
-      {:exit, reason} ->
-        flunk("""
-        command crashed: #{format_command(executable, args)}
-
-        reason:
-        #{inspect(reason)}
-        """)
-
-      {:timeout, timeout} ->
-        flunk("""
-        command timed out after #{timeout}ms: #{format_command(executable, args)}
-        """)
-    end
-  end
-
-  defp assert_normal_container_command_exit!(docker, args, output, exit_status) do
-    cond do
-      exit_status in [125, 126, 127] ->
-        flunk("""
-        docker failed to run the container command: #{format_command(docker, args)}
-
-        exit status:
-        #{exit_status}
-
-        output:
-        #{output}
-        """)
-
-      true ->
-        :ok
     end
   end
 
